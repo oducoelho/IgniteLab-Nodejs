b4fa468a7cd470334e4ea750bc8e0826dbd9f737
Eduardo Coelho <eduardocoelho619@gmail.com>
2022-12-17 23:04:38 -0300

    Aula 03 - Infraestrutura e injeção de dependências

--- src/applications/entidades/notification.ts
+++ src/applications/entidades/notification.ts
@@ -7,15 +7,19 @@ export interface NotificationProps {
   content: Content;
   category: string;
   readAt?: Date | null;
+  canceledAt?: Date | null;
   createdAt: Date;
 }
 
-export class Notification {
+export class Notification { // eslint-disable-line
   private _id: string;
   private props: NotificationProps;
 
-  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
-    this._id = randomUUID();
+  constructor(
+    props: Replace<NotificationProps, { createdAt?: Date }>,
+    id?: string,
+  ) {
+    this._id = id ?? randomUUID();
     this.props = {
       ...props,
       createdAt: props.createdAt ?? new Date(),
@@ -48,12 +52,25 @@ export class Notification {
   public get category(): string {
     return this.props.category;
   }
-  public set readAt(readAt: Date | null | undefined) {
-    this.props.readAt = readAt;
+
+  public read() {
+    this.props.readAt = new Date();
+  }
+
+  public unread() {
+    this.props.readAt = null;
   }
 
   public get readAt(): Date | null | undefined {
     return this.props.readAt;
+  }
+
+  public cancel() {
+    this.props.canceledAt = new Date();
+  }
+
+  public get calceledAt(): Date | null | undefined {
+    return this.props.canceledAt;
   }
 
   public get createdAt(): Date | null | undefined {
